08b8978e29c5ddd1edd3b80af0d2974b56498ffa
Noa Nabeshima <noanabeshima@gmail.com>
2023-11-26 16:22:43 -0800

    replaced title kwarg with label kwarg

--- interp-lib/tok_labelling/tok_labelling.py
+++ interp-lib/tok_labelling/tok_labelling.py
@@ -34,18 +34,18 @@ def format_nest(nest, **kwargs):
 
 prompt_template = '''Given the list of strings below, identify which strings match the following description:
 {description}
-To log the strings that match the description, call log_{title}. Copy strings exactly as they appear.
+To log the strings that match the description, call log_{label}. Copy strings exactly as they appear.
 
 Here is the list of strings for you to process: {batch}'''
 
 functions_template = [
     {
-    "name": "log_{title}",
+    "name": "log_{label}",
     "description": "",
     "parameters": {
         "type": "object",
         "properties": {
-            "identified_{title}": {
+            "identified_{label}": {
                 "type": "array",
                 "description": "{description}",
                 "items": {
@@ -54,22 +54,22 @@ functions_template = [
             },
         },
     },
-    "required": ["log_{title}"],
+    "required": ["log_{label}"],
     },
 ]
 
-def get_tok_strs_from_batch(batch, title, description, model='gpt-4',):
-    out = query(prompt=prompt_template.format(title=title, description=description, batch=batch),
+def get_tok_strs_from_batch(batch, label, description, model='gpt-4',):
+    out = query(prompt=prompt_template.format(label=label, description=description, batch=batch),
                 messages=[],
                 model=model,
-                functions=format_nest(functions_template, title=title, description=description),
-                function_call={'name': f"log_{title}"},
+                functions=format_nest(functions_template, label=label, description=description),
+                function_call={'name': f"log_{label}"},
                 temperature=0.9)
 
     try:
         outputted_words = json.loads(
             out.choices[0].message.function_call.arguments
-        )[f"identified_{title}"]
+        )[f"identified_{label}"]
     except Exception as e:
         print(f'Exception: {e}')
         return []
@@ -87,26 +87,26 @@ def get_tok_strs_from_batch(batch, title, description, model='gpt-4',):
     return words
 
 
-def new_tok_label(title, description, batch_size=50, model='gpt-4'):
+def new_tok_label(label, description, batch_size=50, model='gpt-4'):
     '''
-    title: str, title of label
+    label: str, snake_case short representation of the label
     description: str, describing all tokens that fit the label.
     
     EG
-    new_tok_label(title='past_tense_verbs', description='Strings that are past-tense verbs.')
+    new_tok_label(label='past_tense_verbs', description='Strings that are past-tense verbs.')
     '''
-    assert '-' not in title, 'snake_case please'
-    assert ' ' not in title, 'snake_case please'
-    assert title.lower() == title, 'snake_case please'
+    assert '-' not in label, 'snake_case please'
+    assert ' ' not in label, 'snake_case please'
+    assert label.lower() == label, 'snake_case please'
 
     ls = os.listdir(local_fname(f'tok_labels'))
-    if title+'.json' in ls:
-        with open(local_fname(f'tok_labels/{title}.json'), 'r') as f:
+    if label+'.json' in ls:
+        with open(local_fname(f'tok_labels/{label}.json'), 'r') as f:
             existing_desc = json.load(f)['description']
         if description == existing_desc:
-            print(f'A tok label, "{title}", already exists with this description.')
+            print(f'A tok label, "{label}", already exists with this description.')
             return 
-        inp = input(f'{title}.json already found in tok_labels/ with description:\n{existing_desc}\nOverwrite this file?')
+        inp = input(f'{label}.json already found in tok_labels/ with description:\n{existing_desc}\nOverwrite this file?')
         if inp.strip().lower() not in {'y', 'yes'}:
             print('Aborting tok labelling.')
             return
@@ -120,7 +120,7 @@ def new_tok_label(title, description, batch_size=50, model='gpt-4'):
     for _ in range(3):
         random.shuffle(toks)
         batches = [toks[i:i+batch_size] for i in range(0, len(toks), batch_size)]
-        out = list(itertools.chain.from_iterable(threaded_map(get_tok_strs_from_batch, batches, n_threads=20, kwargs={'title': title, 'description': description, 'model': model})))
+        out = list(itertools.chain.from_iterable(threaded_map(get_tok_strs_from_batch, batches, n_threads=20, kwargs={'label': label, 'description': description, 'model': model})))
 
         for word in out:
             tok_id = tokenizer.encode(word).ids[0]
@@ -130,26 +130,26 @@ def new_tok_label(title, description, batch_size=50, model='gpt-4'):
     identified_words = [tokenizer.decode([word]) for word in identified_words]
     identified_words = list(set(identified_words))
     result = {
-        'title': title,
+        'label': label,
         'description': description,
         'tokens': identified_words
     }
-    with open(local_fname(f'tok_labels/{title}.json'), 'w') as f:
+    with open(local_fname(f'tok_labels/{label}.json'), 'w') as f:
         json.dump(result, f)
-    print(f'Saved {len(identified_words)} words in {title}.json!')
+    print(f'Saved {len(identified_words)} words in {label}.json!')
 
 import os
 
-def load_tok_label(title):
+def load_tok_label(label):
     available_labels = [item.split('.')[0] for item in os.listdir(local_fname('tok_labels')) if item.split('.')[1] == 'json']
-    if title not in available_labels:
-        print(f'tok label "{title}" not found. Available tok labels are: {available_labels}')
+    if label not in available_labels:
+        print(f'tok label "{label}" not found. Available tok labels are: {available_labels}')
     else:
-        with open(local_fname(f'tok_labels/{title}.json'), 'r') as f:
+        with open(local_fname(f'tok_labels/{label}.json'), 'r') as f:
             res = json.load(f)
         description = res['description']
         labelled_toks = res['tokens']
-        print(f'Loading {title}: {description}')
+        print(f'Loading {label}: {description}')
         return labelled_toks
 
 
